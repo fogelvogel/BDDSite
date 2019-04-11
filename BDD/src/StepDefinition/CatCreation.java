8485c0b8c490ba75c7390dfb8af402432e13e399
Elena Popova <44632346+fogelvogel@users.noreply.github.com>
2019-04-11 13:01:27 +0700

    Добавлен тест создания новой кошки

--- BDD/src/StepDefinition/CatCreation.java
+++ BDD/src/StepDefinition/CatCreation.java
@@ -16,42 +16,50 @@ public class CatCreation {
 	
 	@Given("^i opened browser window$")
 	public void i_opened_browser_window() throws Throwable {
+		System.setProperty("webdriver.chrome.driver", "C:\\BDDSite\\chromedriver_win32\\chromedriver.exe");					
+	       driver= new ChromeDriver();					
+	       driver.manage().window().maximize();			
+	       driver.get("http://newbdd/cat/");	
 	  
 	}
 
 	@When("^i clicked new button$")
 	public void i_clicked_new_button() throws Throwable {
+		driver.findElement(By.cssSelector("a")).click();
 	   
 	}
 
 	@When("^i typed \"(.*?)\" in imya field$")
 	public void i_typed_in_imya_field(String arg1) throws Throwable {
-	  
+		driver.findElement(By.cssSelector("#cat_imya")).sendKeys(arg1);
+		
 	}
 
 	@When("^i typed \"(.*?)\" in poroda field$")
 	public void i_typed_in_poroda_field(String arg1) throws Throwable {
-	   
+		driver.findElement(By.cssSelector("#cat_poroda")).sendKeys(arg1);
 	}
 
 	@When("^i typed \"(.*?)\" in cvet field$")
 	public void i_typed_in_cvet_field(String arg1) throws Throwable {
+		driver.findElement(By.cssSelector("#cat_cvet")).sendKeys(arg1);
 	    
 	}
 
 	@When("^i typed \"(.*?)\" in harakter field$")
 	public void i_typed_in_harakter_field(String arg1) throws Throwable {
-	    
+		driver.findElement(By.cssSelector("#cat_harakter")).sendKeys(arg1);
 	}
 
 	@When("^i clicked save button$")
 	public void i_clicked_save_button() throws Throwable {
-	   
+		driver.findElement(By.cssSelector("button")).click();
 	}
 
 	@Then("^new cat should be visible$")
 	public void new_cat_should_be_visible() throws Throwable {
-	  
+		Assert.assertNotNull(driver.findElement(By.cssSelector("td")));
+		 driver.close();
 	}
 
 }
